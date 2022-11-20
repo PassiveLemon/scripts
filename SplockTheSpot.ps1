d074aa205b6a5bc73f0aaecb8b324f11e021173f
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-11-20 15:34:47 -0500

    Update SplockTheSpot.ps1

--- SplockTheSpot.ps1
+++ SplockTheSpot.ps1
@@ -1,9 +1,5 @@
 iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
-pause
 iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex
-pause
 spicetify apply
-pause
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex "& { $((iwr -useb 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content) } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_on -cache_off -block_update_off -exp_standart -hide_col_icon_off -start_spoti"
-pause
